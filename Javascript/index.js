95ca1f943416509fad0a50fabfd6542f6c38b090
xiaofeng-qu <xiaofeng.qu.83@gmail.com>
2019-01-18 11:09:12 -0500

    Add button to reverse text.

--- Javascript/index.js
+++ Javascript/index.js
@@ -29,3 +29,15 @@ var app4 = new Vue({
         ]
     }
 });
+
+var app5 = new Vue({
+    el: "#app-5",
+    data: {
+        message: 'Hello Vue.js!'
+    },
+    methods: {
+        reverseMessage: function(){
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+});
