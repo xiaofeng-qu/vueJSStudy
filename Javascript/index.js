18d13bf8096bb11efe6a5acf1a88cff68786efa3
xiaofeng-qu <xiaofeng.qu.83@gmail.com>
2019-01-18 11:00:53 -0500

    Add a todo list.

--- Javascript/index.js
+++ Javascript/index.js
@@ -18,3 +18,14 @@ var app3 = new Vue({
         seen: true
     }
 });
+
+var app4 = new Vue({
+    el: "#app-4",
+    data: {
+        todos: [
+            { text: 'Learn JavaScript' },
+            { text: 'Leanrn Vue' },
+            { text: 'Build something awesome' }
+        ]
+    }
+});
